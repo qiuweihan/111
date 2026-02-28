f7c8c63995a4c891250da7e08a8ca58ae7efd410
qiuweihan <qiuweihan97@outlook.com>
2026-02-28 17:51:24 +0800

    Update isom5240app.py

--- isom5240app.py
+++ isom5240app.py
@@ -1,27 +1,54 @@
+# import part
 from transformers import pipeline
 from PIL import Image
 import streamlit as st
 
-# Streamlit UI
-st.header("Title: Age Classification using ViT")
 
-# Load the age classification pipeline
-# The code below should be placed in the main part of the program
-age_classifier =pipeline("image-classification", 
-                                   model="akashmaggon/vit-base-age-classification")
+# function part
+def ageClassifier(imgFilename, modelName):
+    # Load the age classification pipeline
+    # The code below should be placed in the main part of the program
+    age_classifier = pipeline(""image-classification"", 
+                              model=modelName)
+    
+    image_name = imgFilename
+    image_name = Image.open(image_name).convert(""RGB"")
+    
+    # Classify age
+    age_predictions = age_classifier(image_name)
+
+    return age_predictions
+
+
+def output_msg(age_predictions):
+    st.write(age_predictions)
+    age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
+    
+    # Display results
+    st.write(""Predicted Age Range:"")
+    st.write(f""Age range: {age_predictions[0]['label']}"")
+    
+    st.write(""Done"")    
 
 
 
-image_name = "middleagedMan.jpg"
-image_name = Image.open(image_name).convert("RGB")
+def main():
+    # Streamlit UI
+    st.header(""Title: Age Classification using ViT"")
+    
 
-# Classify age
-age_predictions = age_classifier(image_name)
-st.write(age_predictions)
-age_predictions = sorted(age_predictions, key=lambda x: x['score'], reverse=True)
 
-# Display results
-st.write("Predicted Age Range:")
-st.write(f"Age range: {age_predictions[0]['label']}")
+#    age_predictions = ageClassifier(""middleagedMan.jpg"",
+#                                   ""prithivMLmods/Age-Classification-SigLIP2"")
+    age_predictions = ageClassifier(""middleagedMan.jpg"",
+                                   ""akashmaggon/vit-base-age-classification"")
+    output_msg(age_predictions)
 
-st.write("Done")
+  
+
+
+
+# main part
+if __name__ == ""__main__"":
+    main()
+
